f810c28a729f28bbce15ff1820de2938eb566fd4
MisaghMomeniB <jenabemisagh@gmail.com>
2025-02-22 22:53:58 +0330

    imports

---
+++ Landing-Pages/Landing-Page-2/src/App.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+import { Coffee, Clock, MapPin, Phone, Instagram, Star, Award, Leaf, Coffee as CoffeeBean, Sun, Moon } from 'lucide-react';
+import { useTheme } from './ThemeContext';
